f6bdddb7e445ebee85d2a095e07ae7fd8651ae12
Ramkumar Dhinakaran <ramkumar.dhinakaran@perficient.com>
2016-05-05 20:30:52 +0530

    Kendo UI Integration

---
+++ MVCAPP/Controllers/FormController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAPP.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: PlanSelectionForm
+        public ActionResult PlanSelection()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCAPP/Models/RegistrationModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCAPP.Models
+{
+    public class RegistrationModel
+    {
+
+        [Required(ErrorMessage = "Please enter an Email")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Contact No")]
+        public string ContactNo { get; set; }
+
+
+
+        [Required]
+        [Display(Name = "Blood Group")]
+        public string BloodGroup { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public System.DateTime DOB { get; set; }
+    }
+}
